f3fcd26ad2b919046aebb4bdad26492b48cd5ed6
fharhanamrin <fharhanamrin028@gmail.com>
2025-04-19 20:19:02 +0700

    chore: clean up project dependencies and unused files
    Remove unused files and dependencies, including TailwindCSS, Node modules, and related configuration files. Add new demo files for testing purposes.

---
+++ demo/demo.js
@@ -0,0 +1,14 @@
+function sayHello() {
+    let name;
+    if (!name) {
+        throw Error("Name is required");
+    }
+}
+
+try {
+    sayHello();
+} catch (error) {
+    console.error("Sorry", error);
+}
+
+console.log("Continue...");
---
+++ script.js
@@ -0,0 +1,64 @@
+const contacts = [
+    {
+        id: 1,
+        name: "M Haidar Hanif",
+        age: 30,
+        email: "haidar@example.com",
+        phone: "+62-851-0193029",
+        address: "Bandung, Indonesia",
+    },
+    {
+        id: 2,
+        name: "Elon Musk",
+        age: 50,
+        email: "elon@example.com",
+        phone: "+1-12121-0193029",
+        address: "California, USA",
+    },
+];
+
+function displayContacts() {
+    for (let index = 0; index < contacts.length; index++) {
+        const contact = contacts[index];
+
+        console.log(`
+ID: ${contact.id}
+👤 Name: ${contact.name}
+📧 Email: ${contact.email}
+📞 Phone: ${contact.phone}
+🎂 Age: ${contact.age} years old
+📍 Address: ${contact.address}
+`);
+    }
+}
+
+function addContact(name, age, email, phone, address) {
+    const lastContact = contacts[contacts.length - 1];
+    const lastId = lastContact.id;
+    const nextId = lastId + 1;
+
+    contacts.push({
+        id: nextId,
+        name,
+        age,
+        email,
+        phone,
+        address,
+    });
+}
+
+function searchContacts() {
+    // ...
+}
+
+function deleteContact() {
+    // ...
+}
+
+function updateContact() {
+    // ...
+}
+
+addContact("Mark Zuck", 40, "mark@example.com", "+123", "Palo Alto, CA, USA");
+addContact("Bill Gates", 60, "bill@example.com", "+123", "Seattle, WA, USA");
+displayContacts();
